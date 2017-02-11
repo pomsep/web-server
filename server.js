dee75a9e6c40645a42bcc81f07253a698e083844
Tassos Seperis <pomsep@gmail.com>
2017-02-11 22:32:34 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+app.get('/', function (req, res) { 
+   res.send('Hello Express... !');
+});
+
+app.get('/about', function (req, res) {
+   res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+   console.log('Express server started on port ' + PORT);
+});
